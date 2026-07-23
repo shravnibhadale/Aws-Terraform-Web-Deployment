7181908b610a74cb3abbeb45e0b07f3203a31da8
Shravani bhadale <shravnicloud@gmail.com>
2026-07-23 18:14:44 +0530

    Delete Aws-Terraform-Web-Deployment directory

--- Aws-Terraform-Web-Deployment/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt update -y
-sudo apt install apache2 -y
-sudo systemctl start apache2
-sudo systemctl enable apache2
-cd /var/www/html
-sudo rm -rf *
-git clone https://github.com/shravnibhadale/Aws-Terraform-Web-Deployment.git .
